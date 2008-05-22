dd19e985a86bbe48c57f87543516c53670caae82
Robert Norris <rob@cataclysm.cx>
2008-05-23 06:41:06 +1000

    imported old code from IO::Prompt

--- lib/Test/MockTerm.pm
+++ lib/Test/MockTerm.pm
@@ -1,107 +1,218 @@
 package Test::MockTerm;
 
+#
+# This module allows testing of interactive programs that also poke at
+# terminal settings. It was specifically designed for testing IO::Prompt, but
+# might be useful elsewhere.
+#
+# IO::Prompt uses the following methods to communicate with the terminal:
+#   - opens /dev/tty for reading        open $IN, "</dev/tty"
+#   - opens /dev/tty for writing        open $OUT, ">/dev/tty"
+#   - tests $IN to see if its           -t $IN
+#   - changes terminal modes            use Term::ReadKey
+#                                       ReadMode 'xyz', $IN
+#   - gets current term control chars   GetControlChars $IN
+#
+# We handle this in the following way:
+#   - open          - override CORE::GLOBAL::open and look for attempts to
+#                     open the terminal device
+#   - -t            - make sure the handles returned from open actually are
+#                     (pseudo) terminals via IO::Pty
+#   - Term::ReadKey - since the handles are terminals already, this just works
+#
+# (This does mean that the tests will only work on systems that have
+# pseudo-terminals. A more portable solution would be to override or otherwise
+# control Perl's -t operator, and then install fake Term::ReadKey functions.
+# As of Perl 5.8.8 there is no way to override -t, so we're stuck with this
+# method for now).
+#
+# To start, you create a Test::MockTerm object, with the file that would
+# normally be opened to get at the terminal.
+#
+#   my $mock = Test::MockTerm->new("/dev/tty");
+#
+# This arranges for Perl's "open" operator to return IO::Pty/IO::Tty objects
+# when something attempts to open the named file for read (<) or write (>).
+#
+# You can "type" input onto the console with $mock->put. Similarly you can
+# read back off the console with $mock->get (for a single char) or
+# $mock->getline (calls the underlying IO::Handle::getline). Neither of these
+# will block; get will return undef if there's nothing waiting to be read.
+#
+# Because its more useful for testing, put and getline will turn \r\n, \n\r
+# and \r sequences into \n. get won't do this though.
+#
+# Be aware that pseudo-ttys (like most real terminals) echo input directly, so
+# if you do ->put then ->get, you'll get back whatever you just wrote. Of
+# course the program on the other end can control echoing.
+#
+
 use warnings;
 use strict;
 
-=head1 NAME
+use IO::Pty;
+use Scalar::Util qw(refaddr weaken);
+use Carp;
 
-Test::MockTerm - The great new Test::MockTerm!
+my %devices;
 
-=head1 VERSION
+sub import {
 
-Version 0.01
+    # override the system open so we can intercept stuff
+    *CORE::GLOBAL::open = sub {
 
-=cut
+        # first figure out what they're trying to do
+        my ($mode, $file);
 
-our $VERSION = '0.01';
+        # three-arg form is easy
+        if (@_ == 3) {
+            $mode = $_[1];
+            $file = $_[2];
+        }
 
+        # two-arg form, just take the first character
+        elsif (@_ == 2) {
+            $mode = substr $_[1], 0, 1;
+            $file = substr $_[1], 1;
+        }
 
-=head1 SYNOPSIS
+        # magical one-arg form, sigh
+        elsif (@_ == 1) {
+            no strict "refs";
+            $mode = "<";
+            $file = ${$_[0]};
+        }
 
-Quick summary of what the module does.
+        # if we couldn't extract a filename, or we're not interested in that
+        # file, just pass it through to the normal open
+        if (not $file or not exists $devices{$file}) {
 
-Perhaps a little code snippet.
+            # first arg can be a symbol
+            if (defined $_[0]) {
 
-    use Test::MockTerm;
+                # since they're bouncing through us, open will get the caller
+                # wrong unless we qualify the handle name
+                use Symbol ();
+                my $handle = Symbol::qualify($_[0], (caller)[0]);
 
-    my $foo = Test::MockTerm->new();
-    ...
+                no strict "refs";
+                if    (@_ == 1) { return CORE::open $handle }
+                elsif (@_ == 2) { return CORE::open $handle, $_[1] }
+                else            { return CORE::open $handle, $_[1], @_[2..$#_] }
+            }
 
-=head1 EXPORT
+            # or a scalar
+            else {
+                if    (@_ == 1) { return CORE::open $_[0] }
+                elsif (@_ == 2) { return CORE::open $_[0], $_[1] }
+                else            { return CORE::open $_[0], $_[1], @_[2..$#_] }
+            }
+        }
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+        # so we're handling this file. if they're trying to do something other
+        # than just read or write, then we're confused
+        if ($mode ne "<" and $mode ne ">") {
+            croak "no support for mode '$mode' for $file";
+        }
 
-=head1 FUNCTIONS
+        # we're going to hand the slave back to them
+        my $tty = $devices{$file}->{pty}->slave;
+        $tty->blocking(0);
 
-=head2 function1
+        # they passed a plain globby symboly thing
+        if (defined $_[0]) {
 
-=cut
+            # see above
+            use Symbol ();
+            my $handle = Symbol::qualify($_[0], (caller)[0]);
 
-sub function1 {
+            no strict "refs";
+            *{$handle} = $tty;
+        }
+
+        # otherwise its a just a scalar, and we can trample it
+        else {
+            $_[0] = $tty;
+        }
+
+        return 1;
+    };
 }
 
-=head2 function2
+sub new {
+    my ($class, @files) = @_;
 
-=cut
+    my %files = map { $_ => 1 } @files;
+    @files = keys %files;
 
-sub function2 {
+    croak "no files specified" if @files == 0;
+
+    if (@files == 1) {
+        return $devices{$files[0]}->{self} if exists $devices{$files[0]};
+    }
+    else {
+        my $count = 0;
+        for my $file (@files) {
+            $count++ if exists $devices{$file};
+        }
+        return $devices{$files[0]}->{self} if $count == 1;
+
+        croak "two or more of the specified files we're previously opened independently, and can't be grouped now" if $count > 1;
+    }
+
+    my $pty = IO::Pty->new;
+    $pty->blocking(0);
+
+    my $self = bless \$pty, $class;
+    my $device = {
+        pty  => $pty,
+        self => $self,
+    };
+    weaken $device->{self};
+
+    $devices{$_} = $device for @files;
+
+    return $self;
 }
 
-=head1 AUTHOR
+sub DESTROY {
+    my ($self) = @_;
+    for my $file (keys %devices) {
+        delete $devices{$file} if refaddr $self == refaddr $devices{$file}->{self};
+    }
+}
 
-Robert Norris, C<< <rob at cataclysm.cx> >>
+sub put {
+    my ($self, @stuff) = @_;
+    map { s/(?:\r\n|\n\r|\r)/\n/g } @stuff;
+    print {$$self} @stuff;
+}
 
-=head1 BUGS
+sub get {
+    my ($self) = @_;
+    return $$self->getc;
+}
 
-Please report any bugs or feature requests to C<bug-test-mockterm at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-MockTerm>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+sub getline {
+    my ($self) = @_;
+    my $line = $$self->getline;
+    return if not defined $line;
+    $line =~ s/(?:\r\n|\n\r|\r)/\n/g;
+    return $line;
+}
 
+#
+# Because I always forget:
+#
+# $master = IO::Pty->new;
+# $slave  = $master->slave;
+#
+# the master of a ptty-pair is where the user would normal sit, ie:
+#   print $master == typing on the keyboard
+#   <$master>     == reading from the screen
+#
+# the SLAVE is the process end. IO::Prompt $IN reads from there, and $OUT
+# writes to there.
+#
 
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Test::MockTerm
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test-MockTerm>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Test-MockTerm>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Test-MockTerm>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Test-MockTerm>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Robert Norris, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of Test::MockTerm
+1;
---
+++ t/02-mockterm.t
@@ -0,0 +1,62 @@
+use warnings;
+use strict;
+
+use Test::More tests => 14;
+
+my $dev = "/dev/term";
+my $dev2 = "-";
+
+BEGIN {
+    use_ok("Test::MockTerm");
+}
+
+my $term = Test::MockTerm->new($dev);
+
+open my ($in), "<", $dev;
+isa_ok($in, "GLOB", "read handle");
+
+open my ($out), ">", $dev;
+isa_ok($out, "GLOB", "write handle");
+
+is($in, $out, "and they're the same");
+
+open my ($in2), "<", $dev;
+is($in, $in2, "opening again for read returns the same object");
+
+open my ($out2), ">", $dev;
+is($out, $out2, "and for write");
+
+undef $in2;
+open $in2, "<$dev";
+is($in, $in2, "two argument form for reads returns the same object");
+
+undef $out2;
+open $out2, ">$dev";
+is($out, $out2, "and for write");
+
+undef $in2; undef $out2;
+
+ok(-t $in, "read handle appears to be connected to a terminal");
+ok(-t $out, "and so does write");
+
+$term->put("flurble\n");
+my $read = <$in>;
+is($read, "flurble\n", '"typing" stuff appears on the other end');
+
+my $echoed = $term->getline;
+is($echoed, "flurble\n", "and gets echoed back");
+
+print $out "wibble\n";
+my $written = $term->getline;
+is($written, "wibble\n", 'stuff written by the other end gets onto the "screen"');
+
+close $in;
+close $out;
+undef $term;
+
+$term = Test::MockTerm->new("/dev/term", "-");
+
+open $in, "<", $dev;
+open $in2, "<", $dev2;
+
+is(fileno $in, fileno $in2, "overriding multiple files then opening returns the same (underlying) handle");
