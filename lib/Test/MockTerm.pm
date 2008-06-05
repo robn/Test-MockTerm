a102e19a347c6a38ee185ac5e9c451d21be1290b
Robert Norris <rob@cataclysm.cx>
2008-06-05 20:06:03 +1000

    override open to return the slave

--- lib/Test/MockTerm.pm
+++ lib/Test/MockTerm.pm
@@ -8,8 +8,91 @@ use warnings;
 use strict;
 
 use Symbol ();
+use Scalar::Util qw(refaddr);
 use Carp;
 use Tie::Handle;
+
+our %bound;
+
+sub import {
+
+    # override the system open so we can intercept stuff
+    *CORE::GLOBAL::open = sub {
+
+        # first figure out what they're trying to do
+        my ($mode, $file);
+
+        # three-arg form is easy
+        if (@_ == 3) {
+            $mode = $_[1];
+            $file = $_[2];
+        }
+
+        # two-arg form, just take the first character
+        elsif (@_ == 2) {
+            $mode = substr $_[1], 0, 1;
+            $file = substr $_[1], 1;
+        }
+
+        # magical one-arg form, sigh
+        elsif (@_ == 1) {
+            no strict "refs";
+            $mode = "<";
+            $file = ${$_[0]};
+        }
+
+        # if we couldn't extract a filename, or we're not interested in that
+        # file, just pass it through to the normal open
+        if (not $file or not exists $bound{$file}) {
+
+            # first arg can be a symbol
+            if (defined $_[0]) {
+
+                # since they're bouncing through us, open will get the caller
+                # wrong unless we qualify the handle name
+                my $handle = Symbol::qualify($_[0], (caller)[0]);
+
+                no strict "refs";
+                if    (@_ == 1) { return CORE::open $handle }
+                elsif (@_ == 2) { return CORE::open $handle, $_[1] }
+                else            { return CORE::open $handle, $_[1], @_[2..$#_] }
+            }
+
+            # or a scalar
+            else {
+                if    (@_ == 1) { return CORE::open $_[0] }
+                elsif (@_ == 2) { return CORE::open $_[0], $_[1] }
+                else            { return CORE::open $_[0], $_[1], @_[2..$#_] }
+            }
+        }
+
+        # so we're handling this file. if they're trying to do something other
+        # than just read or write, then we're confused
+        if ($mode ne "<" and $mode ne ">") {
+            croak "no support for mode '$mode' for $file";
+        }
+
+        # send the slave back
+        my $handle = $bound{$file}->slave;
+
+        # they passed a plain globby symboly thing
+        if (defined $_[0]) {
+
+            # see above
+            my $glob = Symbol::qualify($_[0], (caller)[0]);
+
+            no strict "refs";
+            *{$glob} = $handle;
+        }
+
+        # otherwise its a just a scalar, and we can trample it
+        else {
+            $_[0] = $handle;
+        }
+
+        return 1;
+    };
+}
 
 sub new {
     my ($class, @args) = @_;
@@ -25,15 +108,23 @@ sub new {
     $self->{master} = $master;
     $self->{slave} = $slave;
 
+    $self->{files} = { };
+
     return $self;
 }
 
 sub DESTROY {
     my ($self) = @_;
+
     untie *{$self->{master}};
     delete $self->{master};
+
     untie *{$self->{slave}};
     delete $self->{slave};
+
+    for my $file (keys %{$self->{files}}) {
+        delete $bound{$file} if defined $bound{$file} and refaddr $bound{$file} == refaddr $self;
+    }
 }
 
 sub master {
@@ -46,6 +137,13 @@ sub slave {
     my ($self) = @_;
 
     return $self->{slave};
+}
+
+sub bind {
+    my ($self, @files) = @_;
+
+    $bound{$_} = $self for @files;
+    $self->{files}->{$_} = 1 for @files;
 }
 
 package Test::MockTerm::Master;
